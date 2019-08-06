5eab6d83852102c759bf87df2153cff94bf283e4
Aleksandar Stolikj <stolaaar@gmail.com>
2019-08-06 21:22:31 +0200

    Moving posts data into separate file

--- client/src/components/pages/ServicePage/ServicePage.js
+++ client/src/components/pages/ServicePage/ServicePage.js
@@ -3,32 +3,7 @@ import Card from "../../card/Card";
 import "./ServicePage.css";
 import Divider from "@material-ui/core/Divider";
 import Service from "./service_/Service";
-
-const PostsData = [
-  {
-    category: "News",
-    title: "Cheap flights",
-    text: "CNN purchased Casey Neistat's Beme app for $25million.",
-    image:
-      "http://steffen-s.com/wp-content/uploads/2016/04/NZ-Landscape-3-600x400.jpg"
-  },
-  {
-    category: "Travel",
-    title: "Airlineser club membership",
-    text: "Learn our tips and tricks on living a nomadic lifestyle",
-
-    image:
-      "https://picography.co/wp-content/uploads/2019/06/picography-dark-skies-over-grass-600x400.jpg"
-  },
-  {
-    category: "Development",
-    title: "Popular destinations",
-
-    text: "The first ever decoupled starter theme for React & the WP-API",
-    image:
-      "http://steffen-s.com/wp-content/uploads/2019/02/L8210_CLOUDS_BONETTE_02-600x400.jpg"
-  }
-];
+import { PostsData } from "../../../Data/postsData";
 
 function ServicePage(props) {
   const serviceRef = useRef();

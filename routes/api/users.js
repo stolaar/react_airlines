72841a89148cc36fb3783198b9e60a6c8aa28e55
Stolic <aleksandars@inform.mk>
2019-08-13 13:22:02 +0200

    Middleware added
    Checks if user is authenticated then continue with the api
    Added descriptions to the routes

--- routes/api/users.js
+++ routes/api/users.js
@@ -3,9 +3,18 @@ const router = express.Router();
 
 const auth_controller = require("../../controllers/authController");
 
+//const check_auth = require('../../middleware/checkAuth'); // @desc Checks if user is authenticated then continue
+
+// @route POST /api/users/login
+// @desc Checks for the user in database and returns token
+// @access Public
 router.post("/login", auth_controller.login);
+
 //router.post("/login", (req, res) => LoginController.Login.execute(req, res));
 
+// @route POST /api/users/register
+// @desc Registers user
+// @access Public
 router.post("/register", auth_controller.register);
 
 module.exports = router;

3e48db7c0c85f1ecc9e48d01e7f4b625820edaf0
Stolar <stolaaar@gmail.com>
2019-10-14 18:27:15 +0200

    Dockerfile and docker-compose changes

--- build-docker.sh
+++ build-docker.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
+dockerd-rootless.sh --experimental
 
 export BUILD_NUMBER=$(git describe --tags --always --dirty="-dev")
 export BRANCH_NAME=$(git rev-parse --symbolic-full-name --abbrev-ref HEAD)
-export PORT_NUMBER='5001'
-export JOB_BASE_NAME ='airlines'
+export PORT_NUMBER="5001"
+export JOB_BASE_NAME="airlines"
 
 echo "Starting docker container"
 docker-compose up --build
